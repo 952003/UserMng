f0bf466fc8f6d554ccd7fbd5532c7ce2079a519d
952003 <51296871+952003@users.noreply.github.com>
2022-02-09 18:10:43 +0300

    1

--- UserMng/wwwroot/js/checkbox.js
+++ UserMng/wwwroot/js/checkbox.js
@@ -1,10 +1,6 @@
 ﻿$('#checkBoxMain').click(function () {
-    if ($(this).is(':checked')) {
-        $('#table input:checkbox').prop('checked', true);
-    } else {
-        $('#table input:checkbox').prop('checked', false);
-    }
-};
+    $('#table input:checkbox').prop('checked', $(this).is(':checked') ? true : false);
+});
 $('#table input:checkbox').click(function () {
     $('#checkBoxMain').prop('checked', false);
 });
